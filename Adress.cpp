46ad9925f3ece2c7fe39caf643a6ed6020a895cc
Anokhin A.A. <Andrew.Anokhin@mail.ru>
2026-06-10 15:52:21 +0300

    Добавлены исправленные решения задач Адреса

--- Adress.cpp
+++ Adress.cpp
@@ -10,32 +10,26 @@ private:
     int apartmentNumber;
 
 public:
-    // Конструктор с параметрами
     Address(const std::string& city, const std::string& street, int houseNumber, int apartmentNumber)
         : city(city), street(street), houseNumber(houseNumber), apartmentNumber(apartmentNumber) {}
 
-    // Метод для получения строки с адресом в нужном формате
     std::string getOutputAddress() const {
         return city + ", " + street + ", " + std::to_string(houseNumber) + ", " + std::to_string(apartmentNumber);
     }
 };
 
 int main() {
-    // Открываем файл для чтения
     std::ifstream inputFile("in.txt");
     if (!inputFile.is_open()) {
         std::cerr << "Не удалось открыть файл in.txt" << std::endl;
         return 1;
     }
 
-    // Читаем количество адресов
     int N;
     inputFile >> N;
 
-    // Создаем динамический массив указателей на Address
     Address** addresses = new Address * [N];
 
-    // Читаем адреса из файла
     for (int i = 0; i < N; i++) {
         std::string city, street;
         int houseNumber, apartmentNumber;
@@ -46,24 +40,27 @@ int main() {
 
     inputFile.close();
 
-    // Открываем файл для записи
     std::ofstream outputFile("out.txt");
     if (!outputFile.is_open()) {
-        std::cerr << "Не удалось открыть файл out.txt" << std::endl;  // Убраны лишние скобки
+        std::cerr << "Не удалось открыть файл out.txt" << std::endl;
+
+        // ОСВОБОЖДАЕМ ПАМЯТЬ ПЕРЕД ВЫХОДОМ
+        for (int i = 0; i < N; i++) {
+            delete addresses[i];
+        }
+        delete[] addresses;
+
         return 1;
     }
 
-    // Записываем количество адресов
     outputFile << N << std::endl;
 
-    // Записываем адреса в обратном порядке
     for (int i = N - 1; i >= 0; i--) {
         outputFile << addresses[i]->getOutputAddress() << std::endl;
     }
 
     outputFile.close();
 
-    // Освобождаем память
     for (int i = 0; i < N; i++) {
         delete addresses[i];
     }
--- Adress2.cpp
+++ Adress2.cpp
@@ -10,28 +10,22 @@ private:
     int apartmentNumber;
 
 public:
-    // Конструктор с параметрами
     Address(const std::string& city, const std::string& street, int houseNumber, int apartmentNumber)
         : city(city), street(street), houseNumber(houseNumber), apartmentNumber(apartmentNumber) {}
 
-    // Геттер для города (нужен для сортировки)
     std::string getCity() const {
         return city;
     }
 
-    // Метод для получения строки с адресом в нужном формате
     std::string getOutputAddress() const {
         return city + ", " + street + ", " + std::to_string(houseNumber) + ", " + std::to_string(apartmentNumber);
     }
 };
 
-// Функция сортировки пузырьком по названию города
 void sort(Address** addresses, int size) {
     for (int i = 0; i < size - 1; i++) {
         for (int j = 0; j < size - i - 1; j++) {
-            // Сравниваем названия городов
             if (addresses[j]->getCity() > addresses[j + 1]->getCity()) {
-                // Меняем местами указатели
                 Address* temp = addresses[j];
                 addresses[j] = addresses[j + 1];
                 addresses[j + 1] = temp;
@@ -41,21 +35,17 @@ void sort(Address** addresses, int size) {
 }
 
 int main() {
-    // Открываем файл для чтения
     std::ifstream inputFile("in.txt");
     if (!inputFile.is_open()) {
         std::cerr << "Не удалось открыть файл in.txt" << std::endl;
         return 1;
     }
 
-    // Читаем количество адресов
     int N;
     inputFile >> N;
 
-    // Создаем динамический массив указателей на Address
     Address** addresses = new Address * [N];
 
-    // Читаем адреса из файла
     for (int i = 0; i < N; i++) {
         std::string city, street;
         int houseNumber, apartmentNumber;
@@ -66,27 +56,29 @@ int main() {
 
     inputFile.close();
 
-    // Сортируем адреса по названию города
     sort(addresses, N);
 
-    // Открываем файл для записи
     std::ofstream outputFile("out.txt");
     if (!outputFile.is_open()) {
         std::cerr << "Не удалось открыть файл out.txt" << std::endl;
+
+        // ОСВОБОЖДАЕМ ПАМЯТЬ ПЕРЕД ВЫХОДОМ
+        for (int i = 0; i < N; i++) {
+            delete addresses[i];
+        }
+        delete[] addresses;
+
         return 1;
     }
 
-    // Записываем количество адресов
     outputFile << N << std::endl;
 
-    // Записываем отсортированные адреса
     for (int i = 0; i < N; i++) {
         outputFile << addresses[i]->getOutputAddress() << std::endl;
     }
 
     outputFile.close();
 
-    // Освобождаем память
     for (int i = 0; i < N; i++) {
         delete addresses[i];
     }
