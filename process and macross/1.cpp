320693de429fa8dc2f8da683ee636c4d22a895ea
Anokhin A.A. <Andrew.Anokhin@mail.ru>
2026-06-26 22:16:19 +0300

    задачи процесс и макрос

---
+++ process and macross/1.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+#define MODE 1
+
+#ifndef MODE
+#error "Необходимо определить MODE"
+#endif
+
+// Объявляем функцию add только если MODE == 1
+#if MODE == 1
+int add(int a, int b) {
+    return a + b;
+}
+#endif
+
+int main() {
+    
+#if MODE == 0
+    std::cout << "Работаю в режиме тренировки" << std::endl;
+#elif MODE == 1
+    std::cout << "Работаю в боевом режиме" << std::endl;
+
+    int num1, num2;
+    std::cout << "Введите число 1: ";
+    std::cin >> num1;
+    std::cout << "Введите число 2: ";
+    std::cin >> num2;
+
+    int result = add(num1, num2);
+    std::cout << "Результат сложения: " << result << std::endl;
+#else
+    std::cout << "Неизвестный режим. Завершение работы" << std::endl;
+#endif
+
+    return 0;
+}
